03ff64aec28610c4f4221553d4ae926b4e8c4f9b
Sam Smith <s.smith@chscreative.com>
2016-06-10 15:16:54 +0100

    driver page
    added driver page and images

--- app/js/services.js
+++ app/js/services.js
@@ -1,5 +1,5 @@
-angular.module('F1FeederApp.services', []).
-  factory('ergastAPIservice', function($http) {
+angular.module('F1FeederApp.services', [])
+  .factory('ergastAPIservice', function($http) {
 
     var ergastAPI = {};
 
@@ -7,6 +7,20 @@ angular.module('F1FeederApp.services', []).
       return $http({
         method: 'JSONP', 
         url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    ergastAPI.getDriverDetails = function(id) {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    ergastAPI.getDriverRaces = function(id) {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
       });
     }
 
